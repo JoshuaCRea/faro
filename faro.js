cbc197cf33f738e809a03ad00fa5008dc3b9765a
Joshua Rea <josh.rea@gmail.com>
2022-12-13 13:07:25 -0500

    Renew Deck function operational, including shuffle and flipping first card.

--- faro.js
+++ faro.js
@@ -1,22 +1,36 @@
+const starterDeck = [
+    '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC',
+    '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH',
+    '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS',
+    '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD'
+];
 
+let cardsOnTable = {
+    flipped: [],
+    unflipped: [],
+}
 
+function _renewDeck(deck) {
+    _getShuffledDeck(deck);
+    cardsOnTable.flipped.push(deck.shift());
+    cardsOnTable.unflipped.push(deck);
+}
 
-function _getShuffledDeck() {
-    const newDeck = [
-        '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC',
-        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH',
-        '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS',
-        '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD'
-    ];
-    for (let i = 0; i < newDeck.length; i++) {
-        const tempCard = newDeck[i];
-        const randomIndex = Math.floor(Math.random() * newDeck.length);
-        newDeck[i] = newDeck[randomIndex];
-        newDeck[randomIndex] = tempCard;
+//function that deals one lose card (first) and one win card (second)
+
+function _getShuffledDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        const tempCard = deck[i];
+        const randomIndex = Math.floor(Math.random() * deck.length);
+        deck[i] = deck[randomIndex];
+        deck[randomIndex] = tempCard;
     }
-    return newDeck;
+    return deck;
 }
 
 window.onload = () => {
-
+    _renewDeck(starterDeck);
+    console.log(cardsOnTable.unflipped);
+    console.log(cardsOnTable.flipped);
 }
+
