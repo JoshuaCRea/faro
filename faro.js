694426c74c90609f14e9f8d75619211f590cbdce
Joshua Rea <josh.rea@gmail.com>
2022-12-13 21:28:52 -0500

    Corrected reference vs. value error in renew function

--- faro.js
+++ faro.js
@@ -5,30 +5,35 @@ const starterDeck = [
     '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD'
 ];
 
-let cardsOnTable = {
+const cardsOnTable = {
     flipped: [],
     unflipped: [],
 }
 
 function _renewDeck(deck) {
-    _getShuffledDeck(deck);
-    cardsOnTable.flipped.push(deck.shift());
-    cardsOnTable.unflipped.push(deck);
+    let newDeck = _shuffleDeck(deck);
+    cardsOnTable.flipped = [];
+    cardsOnTable.flipped.push(newDeck.shift());
+    cardsOnTable.unflipped.push(newDeck);
 }
 
 //function that deals one lose card (first) and one win card (second)
 
-function _getShuffledDeck(deck) {
+function _shuffleDeck(deck) {
     for (let i = 0; i < deck.length; i++) {
         const tempCard = deck[i];
         const randomIndex = Math.floor(Math.random() * deck.length);
         deck[i] = deck[randomIndex];
         deck[randomIndex] = tempCard;
     }
-    return deck;
+    return deck.slice();
 }
 
 window.onload = () => {
+    _renewDeck(starterDeck);
+    console.log(cardsOnTable.unflipped);
+    console.log(cardsOnTable.flipped);
+
     _renewDeck(starterDeck);
     console.log(cardsOnTable.unflipped);
     console.log(cardsOnTable.flipped);
